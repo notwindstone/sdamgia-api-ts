c1cfeeb6c22c0654034f00d58e55c5362fa9150e
notwindstone <notwindstone@gmail.com>
2025-01-21 21:06:19 +0300

    chore(deps): add node-html-parser

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
+import axios from "axios";
+
 const url = "https://rus-ege.sdamgia.ru/";
+
+const fetchHtml = async (url: string) => {
+    const data = axios.get(url);
+    console.log((await data).data);
+}
+
+console.log(fetchHtml(url));
