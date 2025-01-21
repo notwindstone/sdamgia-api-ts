19fa9d15d33c23f90b32fe151417ca3c223cdf48
notwindstone <notwindstone@gmail.com>
2025-01-21 21:37:36 +0300

    feat: save point

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
+import parse from "node-html-parser";
 
-const url = "https://rus-ege.sdamgia.ru/";
+const url = "https://rus-ege.sdamgia.ru/test?id=46230713";
 
 const fetchHtml = async (url: string) => {
-    const data = axios.get(url);
-    console.log((await data).data);
+    const promise = axios.get(url);
+    const data = (await promise).data;
+    const parsedHtml = parse(data);
+    const textElements = parsedHtml.querySelectorAll('p .left_margin');
+    console.log(textElements[0])
+    const content = textElements.map((elem) => {
+        return elem.text;
+    });
+
+    console.log(content);
+
+    return parsedHtml;
 }
 
 console.log(fetchHtml(url));
